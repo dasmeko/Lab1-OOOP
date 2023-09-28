1e27465c802bd69bd97cccfdc6708b7f62aac788
dasmeko <dashadadaday@gmail.com>
2023-09-28 19:58:34 +0300

    Created getValue, setValue and display methods

--- SparseMatrices/Matrix.h
+++ SparseMatrices/Matrix.h
@@ -1,5 +1,113 @@
 #pragma once
 
-class Matrix {
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+template<typename T>
+struct YaleFormat {
+	vector<T> values;
+	vector<T> colIndeces;
+	vector<T> rowDividers;
+};
+
+template<typename T>
+class Matrix {
+private:
+	int rowSize;
+	int colSize;
+public:
+	bool isYaleFormat;
+
+	vector<vector<pair<int, T>>> dataLIL;
+	YaleFormat<T> dataYale;
+
+	Matrix(const int& m, const int& n) {
+		isYaleFormat = false;
+		rowSize = m;
+		colSize = n;
+		dataLIL = vector<vector<pair<int, T>>>(m, vector<pair<int, T>>());
+	}
+
+	void setValue(const int& i, const int& j, const T& val) {
+		if (i < 0 || i >= rowSize || j < 0 || j >= colSize) {
+			throw invalid_argument("Indeces out of bounds");
+		}
+
+		if (isYaleFormat) {
+			//???
+		}
+		else {
+			vector<pair<int, T>> row = dataLIL[i];
+			int k = 0;
+			while (k < row.size()) {
+				if (row[k].first > j) {
+					break;
+				}
+				k++;
+			}
+
+			if (k == row.size()) {
+				dataLIL[i].push_back(pair<int, T>(j, val));
+			}
+			else {
+				dataLIL[i].insert(dataLIL[i].begin() + k, pair<int, T>(j, val));
+			}
+		}
+	}
+
+	T getValue(const int& i, const int& j) {
+		if (i < 0 || i >= rowSize || j < 0 || j >= colSize) {
+			throw invalid_argument("Indeces out of bounds");
+		}
+
+		if (isYaleFormat) {
+			// ???
+		}
+		else {
+			for (pair<int, T> p : dataLIL[i]) {
+				if (p.first == j) {
+					return p.second;
+				}
+			}
+
+			return 0;
+		}
+	}
+
+	void display() {
+		if (isYaleFormat) {
+			// ???
+		}
+		else {
+			for (int i = 0; i < rowSize; i++) {
+				int k = 0;
+
+				if (dataLIL[i].size()) {
+					for (int j = 0; j < dataLIL[i].size(); j++) {
+						while (k < dataLIL[i][j].first) {
+							cout << 0 << '\t';
+							k++;
+						}
+
+						cout << dataLIL[i][j].second << '\t';
+						k++;
+					}
+
+					while (k < colSize) {
+						cout << 0 << '\t';
+						k++;
+					}
+				}
+				else {
+					for (int j = 0; j < colSize; j++) {
+						cout << 0 << '\t';
+					}
+				}
+
+				cout << endl;
+			}
+		}
+	}
 };
--- SparseMatrices/SparseMatrices.cpp
+++ SparseMatrices/SparseMatrices.cpp
@@ -1,5 +1,14 @@
-#include <iostream>
+#include "Matrix.h"
 
 int main() {
+    Matrix<int> m(4, 6);
+    m.setValue(1, 2, 45);
+    m.setValue(3, 4, 12);
+    m.setValue(0, 3, -56);
+    m.setValue(2, 0, 3);
+    m.setValue(0, 1, -4);
+
+    m.display();
+
     return 0;
 }
