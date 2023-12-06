02e27c27595eb284a32aebd3392c1f6145039d44
dasmeko <dashadadaday@gmail.com>
2023-12-06 02:17:25 +0200

    Added line with line intersect

--- SparseMatrices/Figures.h
+++ SparseMatrices/Figures.h
@@ -1,9 +1,25 @@
 #pragma once
 
+#include <vector>
+
+using namespace std;
+
 class Line {
 	double a;
 	double b;
 	double c;
+
+public:
+	vector<pair<double, double>> intersect(Line other) {
+		double den = other.b * a - other.a * b;
+		if (den == 0) {
+			return vector<pair<double, double>>();
+		}
+		else {
+			double num = other.a * c - other.c * a;
+			return vector<pair<double, double>>{pair<double, double>(-(c * den + b * num) / (a * den), num / den)};
+		}
+	}
 };
 
 class Circle {
